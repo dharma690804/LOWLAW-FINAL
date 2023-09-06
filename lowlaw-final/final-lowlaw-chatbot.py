a31e61ee32bbf3d73dcff59c02d0f7c544bf20d9
itsjaeya <135799134+itsjaeya@users.noreply.github.com>
2023-09-06 17:17:51 +0900

    Update final-lowlaw-chatbot.py

--- lowlaw-final/final-lowlaw-chatbot.py
+++ lowlaw-final/final-lowlaw-chatbot.py
@@ -9,14 +9,7 @@ from elastic_app_search import Client
 import json
 import datetime
 
-# Elasticsearch 클라이언트 설정
-es_cloud_id = "lowlaw:YXAtbm9ydGhlYXN0LTIuYXdzLmVsYXN0aWMtY2xvdWQuY29tOjQ0MyQ2YzNmMjA4MmNiMzk0M2YxYTBiZWI0ZDY2M2JmM2VlZCRjZTA2NGZhNjFiMmI0N2Y0ODgzMjY0Y2FlMzVlZDgxZQ=="
-es_username = "elastic"
-es_pw = "LWkW2eILoZYZylsDDThLaCKY"
-
-es = Elasticsearch(cloud_id=es_cloud_id, basic_auth=(es_username, es_pw))
-
-# Appsearch 클라이언트 설정
+# Create App Search client
 client = Client(
     base_endpoint="lowlaw.ent.ap-northeast-2.aws.elastic-cloud.com/api/as/v1",
     api_key="private-egnzqo7tt7fd6fngz13mmox9",
