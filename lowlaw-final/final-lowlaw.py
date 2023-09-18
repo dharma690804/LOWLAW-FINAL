3a269f04f74df47c33aef5b9e9170cad254ed211
dharma690804 <144787271+dharma690804@users.noreply.github.com>
2023-09-18 15:03:56 +0900

    Update final-lowlaw.py

--- lowlaw-final/final-lowlaw.py
+++ lowlaw-final/final-lowlaw.py
@@ -3,4 +3,5 @@ from streamlit_option_menu import option_menu
 from PIL import Image # 이미지
 import os
 
-st.write(os.getcwd())
+st.write("현재 경로: ", os.getcwd())
+st.write("../ 경로: ", os.path.abspath("../"))
